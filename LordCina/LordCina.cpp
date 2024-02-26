22f685865b44c06ce21f2b0dc17be1139a59fcfc
spongebar-netizen <fatanjrz1@gmail.com>
2024-02-26 11:40:27 +0700

    display 'Masukkan jejari='

--- LordCina/LordCina.cpp
+++ LordCina/LordCina.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" <<endl;
-    cout << "Hello";
+    //begin
+       // numeric njejari
+       // display 'Masukkan jejari='
+       // accept njejari
+       // compute nLuas = 3,14 * r * r
+       // display 'Luasnya= ' + nLuas
+    //end
+    double nJejari, nLuas;
+    cout << "Masukkan Jejari lingkaran = ";
+
     system("pause");
 }
